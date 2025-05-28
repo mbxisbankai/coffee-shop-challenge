29e019de7551720fc025594733a8486fd0c9a465
Roy Mbui <108576900+mbxisbankai@users.noreply.github.com>
2025-05-28 07:22:58 +0300

    Update order.py

--- order.py
+++ order.py
@@ -11,26 +11,20 @@ class Order:
     
         from customer import Customer
         from coffee import Coffee
+
+
+        if not isinstance(customer, Customer):
+            raise TypeError('Customer must be an instance of the Customer Class.')
+
+        if not isinstance(coffee, Coffee):
+            raise TypeError('Coffee must be an instance of the Coffee Class.')
+
+        if not isinstance(price, (int, float)) and 10 <= price <= 100:
+            raise ValueError('Please enter a valid price.')
+            
         self._coffee = coffee
         self._customer = customer
         self.price = price
-
-        if isinstance(customer, Customer):
-            self._customer = customer
-            customer.orders.append(self)
-        else:
-            raise TypeError('Customer must be an instance of the Customer Class.')
-
-        if isinstance(coffee, Coffee):
-            self._coffee = coffee
-            coffee.orders.append(self)
-        else:
-            raise TypeError('Coffee must be an instance of the Coffee Class.')
-
-        if isinstance(price, (int, float)) and 10 <= price <= 100:
-            self.price = price
-        else:
-            raise ValueError('Please enter a valid price.')
 
         if coffee not in customer.coffees:
             customer.coffees.append(coffee)
