6cbe9114f06fb4fa4e8b27008fd4685f70bfeb54
Roy Mbui <108576900+mbxisbankai@users.noreply.github.com>
2025-05-28 07:27:32 +0300

    Update order.py

--- order.py
+++ order.py
@@ -7,24 +7,20 @@ if TYPE_CHECKING:
 class Order:
     num_of_orders = 0
 
-    def __init__(self, customer, coffee, price):
-    
-        from customer import Customer
-        from coffee import Coffee
-
-
+    def __init__(self, customer: 'Customer', coffee: 'Coffee', price: float):
         if not isinstance(customer, Customer):
-            raise TypeError('Customer must be an instance of the Customer Class.')
-
+            raise TypeError('Customer must be an instance of the Customer class.')
         if not isinstance(coffee, Coffee):
-            raise TypeError('Coffee must be an instance of the Coffee Class.')
+            raise TypeError('Coffee must be an instance of the Coffee class.')
+        if not (isinstance(price, (int, float)) and 10 <= price <= 100):
+            raise ValueError('Please enter a valid price between 10 and 100.')
 
-        if not isinstance(price, (int, float)) and 10 <= price <= 100:
-            raise ValueError('Please enter a valid price.')
-            
-        self._coffee = coffee
         self._customer = customer
+        self._coffee = coffee
         self.price = price
+
+        customer.orders.append(self)
+        coffee.orders.append(self)
 
         if coffee not in customer.coffees:
             customer.coffees.append(coffee)
@@ -34,10 +30,9 @@ class Order:
         Order.num_of_orders += 1
 
     @property
-    def coffee(self):
-        return self._coffee
-
-    @property
     def customer(self):
         return self._customer
 
+    @property
+    def coffee(self):
+        return self._coffee
